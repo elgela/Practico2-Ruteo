4a9b6f6bd6102bd18d715daadcfa0cf9807ed023
Marcelo Gelato <gelatomarcelo77@gmail.com>
2025-09-17 12:27:22 -0300

    creado

---
+++ ejercicio4/templates/header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <base href="<?php echo 'BASE_URL' ?>" />
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Práctico2-Ruteo</title>
+</head>
+
+<body>
+
+</body>
+
+</html>
