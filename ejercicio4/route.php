710b22f2f2fb36ebadcd41546ff5d85241b7dab2
Marcelo Gelato <gelatomarcelo@hotmail.com>
2025-09-19 01:06:34 -0300

    modificado

--- ejercicio4/route.php
+++ ejercicio4/route.php
@@ -1,50 +1,55 @@
-<?php
+<html>
+<head></head>
+<body>
+    <?php
 
-define('BASE_URL', '//'.$_SERVER['SERVER_NAME'] . ':' . $_SERVER['SERVER_PORT'] . dirname($_SERVER['PHP_SELF']).'/');
+    define('BASE_URL', '//' . $_SERVER['SERVER_NAME'] . ':' . $_SERVER['SERVER_PORT'] . dirname($_SERVER['PHP_SELF']) . '/');
 
 
-// accion que viene por parametro
-$action = 'home'; // por defecto
+    // accion que viene por parametro
+    $action = 'home'; // por defecto
 
-if (!empty($_GET['action'])) { // la reemplazamos si viene definida
-    $action = $_GET['action'];
-} else {
-    $action = 'home'; // acción por defecto si no envían
-}
+    if (!empty($_GET['action'])) { // la reemplazamos si viene definida
+        $action = $_GET['action'];
+    } else {
+        $action = 'home'; // acción por defecto si no envían
+    }
 
-// parsea la acción
-$params = explode("/", $action);
+    // parsea la acción
+    $params = explode("/", $action);
 
-// determina que camino seguir según la acción
-switch($params[0]) {
-    case 'usuarios':
-        if (isset($params[1])) {
-            getUsuarios($params[1]);
-        } else {
-            getUsuarios();
+    // determina que camino seguir según la acción
+    switch ($params[0]) {
+        case 'usuarios':
+            if (isset($params[1])) {
+                getUsuarios($params[1]);
+            } else {
+                getUsuarios();
+            }
+            break;
+        case 'nuevoUsuario':
+            newUsuario();
+            break;
+        case 'dni':
+            if (isset($params[1])) {
+                showUsuario($params[1]);
+            } else {
+                showUsuario();
+            }
+            break;
+    }
+
+    function getUsuarios() {
+        if ($_SERVER['REQUEST_METHOD'] === 'POST' && !empty($_POST['dni'])) {
+            $dni = $_POST['dni'];
         }
-        break;
-    case 'nuevoUsuario':
-        newUsuario();
-        break;
-    case 'dni':
-        if (isset($params[1])) {
-            showUsuario($params[1]);
-        } else {
-            showUsuario();
-        }
-        break;
-        
-}
+    }
 
-function getUsuarios() {
+    function newUsuario() {}
 
-}
+    function showUsuario() {}
 
-function newUsuario() {
+    ?>
+</body>
 
-}
-
-function showUsuario() {
-
-}
+</html>
